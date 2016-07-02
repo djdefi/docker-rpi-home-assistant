227c3f7cc411c1fa9962cb407bdfe9ab55c5e9ee
Ryan Trauntvein <djdefi@users.noreply.github.com>
2016-07-02 14:40:20 -0700

    rpi data path

--- create.sh
+++ create.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8123:8123 --name hass_container -v /home/pi/docker/home-assistant/data:/data home-assistant
+docker run -d -p 8123:8123 --name hass_container -v /home/pirate/home-assistant/data:/data home-assistant
